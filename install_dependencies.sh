a9614dfdf78a5063f68904cecb3c9ba09c39e10d
Moon Junoh <snowphone@lab-pc.localdomain>
2019-04-07 02:18:59 +0900

    Add requirements list

---
+++ install_dependencies.sh
@@ -0,0 +1,16 @@
+ sudo apt install -y  python2.7 python2.7-dev python-virtualenv libusb-dev\
+  libjpeg-dev libtiff-dev libqt4-core rubygems ruby-rmagick libmagickwand-dev\
+  ruby-hpricot scantailor ocrodjvu  djvulibre-bin libffi-dev libjpeg8-dev \
+
+
+
+ sudo apt-get install ruby-dev
+ sudo apt-get install imagemagick
+ sudo apt-get install graphicsmagick
+ sudo apt-get install libmagickcore-dev
+ sudo gem install rmagick
+ sudo gem install iconv
+ sudo gem install pdfbeads
+
+ 
+sudo pip install sexpdata
