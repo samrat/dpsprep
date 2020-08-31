d9646bf101fda1ff42e348c569b0deba9cef843f
Junoh Moon <mjo970625@gmail.com>
2020-08-31 03:54:37 +0000

    Ignore sudo when the user is root

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,16 +1,18 @@
- sudo apt install -y  python2.7 python2.7-dev python-virtualenv libusb-dev\
-  libjpeg-dev libtiff-dev libqt4-core rubygems ruby-rmagick libmagickwand-dev\
-  ruby-hpricot scantailor ocrodjvu  djvulibre-bin libffi-dev libjpeg8-dev \
+if [[ $(whoami) == root ]]; then
+	sudo=""
+else
+	sudo="sudo"
+fi
 
-
-
- sudo apt-get install ruby-dev
- sudo apt-get install imagemagick
- sudo apt-get install graphicsmagick
- sudo apt-get install libmagickcore-dev
- sudo gem install rmagick
- sudo gem install iconv
- sudo gem install pdfbeads
-
- 
-sudo pip install sexpdata
+$sudo apt install -y  python2.7 python-pip python2.7-dev python-virtualenv libusb-dev\
+  libjpeg-dev libtiff-dev ruby ruby-rmagick libmagickwand-dev\
+  ruby-hpricot scantailor ocrodjvu  djvulibre-bin libffi-dev libjpeg8-dev && \
+$sudo apt-get install -y ruby-dev && \
+$sudo apt-get install -y imagemagick && \
+$sudo apt-get install -y graphicsmagick && \
+$sudo apt-get install -y libmagickcore-dev && \
+$sudo gem install rmagick && \
+$sudo gem install iconv && \
+$sudo gem install pdfbeads && \
+$sudo pip install sexpdata && \
+echo "Done!"
